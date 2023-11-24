28ef076cb376ddd4836dc94d34e005986c329aee
Manuel Richard Popp <34488159+ManuelPopp@users.noreply.github.com>
2023-11-24 21:44:11 +0100

    Add files via upload

---
+++ src/graphab4py/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 24 21:43:08 2023
+"""
+__author__ = "Manuel"
+__date__ = "Fri Nov 24 21:43:08 2023"
+__credits__ = ["Manuel R. Popp"]
+__license__ = "Unlicense"
+__version__ = "1.0.1"
+__maintainer__ = "Manuel R. Popp"
+__email__ = "requests@cdpopp.de"
+__status__ = "Development"
+
+#-----------------------------------------------------------------------------|
